33cc318a51e12afa58fd11a93a848b38c9c0d1b8
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-08-24 11:03:00 -0400

    Character Class Skeleton

---
+++ src/main/java/Character.java
@@ -0,0 +1,30 @@
+public class Character {
+
+    int health;
+    int attack;
+    int defense;
+    int speed;
+
+    public Character(){
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
