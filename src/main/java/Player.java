e8c5ecc7cf105e9bfbfde55f666dad6c68ae62ed
Jonathan Capparell <cappytwoj@gmail.com>
2024-02-23 20:13:16 -0500

    dont need to make the same list multiple times

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Player {
+public class Player
+{
     public ArrayList<Character> team =  new ArrayList<>(4);
-    public void createTeam() {
-        CharacterList characterList1  = new CharacterList();
-        CharacterList characterList2  = new CharacterList();
-        CharacterList characterList3  = new CharacterList();
-        team.add(characterList1.pH);
-        characterList1.createCharList();
-        characterList2.createCharList();
-        characterList3.createCharList();
-        team.add(characterList1.charList.get(new Random().nextInt(characterList1.charList.size())));
-        team.add(characterList2.charList.get(new Random().nextInt(characterList2.charList.size())));
-        team.add(characterList3.charList.get(new Random().nextInt(characterList3.charList.size())));
+
+    public void createTeam()
+    {
+        CharacterList characterList  = new CharacterList();
+
+        team.add(characterList.pH);
+        characterList.createCharList();
+
+        team.add(characterList.charList.get(new Random().nextInt(characterList.charList.size())));
+        team.add(characterList.charList.get(new Random().nextInt(characterList.charList.size())));
+        team.add(characterList.charList.get(new Random().nextInt(characterList.charList.size())));
     }
 
     public void removeChar(int charI)
